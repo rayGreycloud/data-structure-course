7b6e0023726ed0d55a8b856a67c824e897342eb5
rayGreycloud <rayGreycloud@gmail.com>
2017-05-19 09:20:51 -0700

    Create project with class for 1D array demo

---
+++ src/com/ds/OneDArray.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.ds;
+
+/**
+ * @author Ray
+ *
+ */
+public class OneDArray {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
