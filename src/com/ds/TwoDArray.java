a9ac85336484501f7ac4e97dd31f242c5863bde5
rayGreycloud <rayGreycloud@gmail.com>
2017-05-23 13:51:34 -0700

    Add condition to print only main row

--- src/com/ds/TwoDArray.java
+++ src/com/ds/TwoDArray.java
@@ -17,7 +17,9 @@ public class TwoDArray {
 		
 		for(int i=0; i<3; i++) {
 			for(int j=0; j<3; j++) {
-				System.out.print(data[i][j] + "\t");
+				if(i==j) {
+					System.out.print(data[i][j] + "\t");
+				}
 			}
 			System.out.println("\n");
 		}
