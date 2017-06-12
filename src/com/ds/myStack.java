58b048f8272b9fc20dc59b43f5397341e83eabe2
rayGreycloud <rayGreycloud@gmail.com>
2017-06-12 13:38:24 -0700

    Collection using Stack

---
+++ src/com/ds/myStack.java
@@ -0,0 +1,18 @@
+package com.ds;
+
+import java.util.Stack;
+
+public class myStack {
+
+	public static void main(String[] args) {
+		
+		Stack<String> st=new Stack<String>();
+		st.push("Admins");
+		st.push("Manager");
+		st.push("Tester");
+		
+		System.out.println(st.pop());
+
+	}
+
+}
