9930fa97569b0980829b23e71fce200693af1d93
rayGreycloud <rayGreycloud@gmail.com>
2017-06-28 09:15:31 -0700

    Implementation of Binary Search

---
+++ src/com/search/BinarySearch.java
@@ -0,0 +1,38 @@
+package com.search;
+
+public class BinarySearch {
+
+	public static void main(String[] args) {
+		DataSet data = new DataSet(1000000);
+		int Search = 999999;
+		Boolean IsFound = false;
+		int low = 0;
+		int high = data.getSize() - 1;
+		int mid = 0;
+
+		System.out.println("Target: " + Search);
+		
+		while (!IsFound) {
+			if (low > high) {
+				System.out.println("Target not found");
+				break;
+			}
+			
+			data.NumberTry++;
+			mid = low + ((high - low) / 2);
+			
+			if (data.data[mid] == Search) {
+				System.out.println("Target found after " + data.NumberTry + " tries");
+				break;
+			}
+			
+			if (data.data[mid] < Search) {
+				low = mid + 1;				
+			}
+			
+			if (data.data[mid] > Search) {
+				high = mid - 1;
+			}
+		}
+	}
+}
